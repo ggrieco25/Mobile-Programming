1229dba0e1b9e760f5fe8f651480693bcb366488
ggrieco25 <ggrieco470@gmail.com>
2025-05-28 10:48:34 +0200

    Creata HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:progetto_mp/HomeScreen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'TV Tracker',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
